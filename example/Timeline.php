fa11088b0fd0a1ebc1e5f499481bece23f31828c
Galih Akbar Moerbayaksa <galnetwork.s@gmail.com>
2017-01-27 22:28:36 +0700

    Create Timeline.php
    Add Example

---
+++ example/Timeline.php
@@ -0,0 +1,9 @@
+<?php
+
+require '../src/PATH.php';
+
+$app = new namespace\API();
+$app->login('email', 'password');
+$app->likeAllTimeline(5, "love", function($data){
+    echo "Sukses Likes\n";
+});
